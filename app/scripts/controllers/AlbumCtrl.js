6f3583bb1fd93b09f60bf63a8442613e69655eda
Danny Diaz <danieldial129@gmail.com>
2017-05-18 20:43:41 -0700

    Services 6. Part 1

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,10 @@
 (function() {
-    function AlbumCtrl() {
-        
+    function AlbumCtrl(Fixtures) {
+        this.albumData = Fixtures.getAlbum();
     }
     
     angular
         
         .module('blocJams')
-        .controller('AlbumCtrl', AlbumCtrl);
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
